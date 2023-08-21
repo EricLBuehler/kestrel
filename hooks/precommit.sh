2755830b45e934af08ef1b467094fea273a98653
EricLBuehler <ericlbuehler@gmail.com>
2023-08-20 21:00:28 -0400

    Update precommit hook

--- hooks/precommit.sh
+++ hooks/precommit.sh
@@ -1,36 +1,38 @@
 #!/bin/bash
 
-# An example hook script to verify what is about to be committed.
-# Called by "git commit" with no arguments.  The hook should
-# exit with non-zero status after issuing an appropriate message if
-# it wants to stop the commit.
+arg="'$*'"
 
 cur=$(pwd)
 mkdir /tmp/kestel_precommit 2> /dev/null
 cp . /tmp/kestrel_precommit -r 2> /dev/null
 cd /tmp/kestrel_precommit
 
-echo "Checking for typos..."
-if typos; then
-	echo "    No typos found: ✔️"
-else
-	echo "Typos failed: ❌"
-	exit 1
-fi
-
-echo "Running KATS..."
-res=$(python3 tests/kats.py)
-return=$(echo $?)
-if ((return==0)); then
-	echo "    KATS passed: ✔️"
-else
-	echo "KATS failed: ❌"
-	exit 1
-fi
-
 echo "Running precommit script..."
-if python3 hooks/precommit.py; then
+python3 hooks/precommit.py $arg
+return=$(echo $?)
+if [ $return -eq 0 ]; then
 	echo "    Precommit passed: ✔️"
+
+    echo "Checking for typos..."
+    if typos; then
+        echo "    No typos found: ✔️"
+    else
+        echo "Typos failed: ❌"
+        exit 1
+    fi
+
+    echo "Running KATS..."
+    res=$(python3 tests/kats.py)
+    return=$(echo $?)
+    if (return==0); then
+        echo "    KATS passed: ✔️"
+    else
+        echo "KATS failed: ❌"
+        exit 1
+    fi
+
+elif [ $return -eq 2 ]; then
+	echo "    Precommit passed (ci-skip): ✔️"
 else
 	echo "Precommit failed: ❌"
 	exit 1
