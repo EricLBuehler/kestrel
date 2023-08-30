e2d78b24b7e7b2486816419023f71f0c85412773
EricLBuehler <ericlbuehler@gmail.com>
2023-08-30 12:36:46 -0400

    Check for todos

--- hooks/precommit.py
+++ hooks/precommit.py
@@ -1,4 +1,5 @@
 import sys
+import subprocess
 
 """
 .git/hooks/commit-msg
@@ -17,4 +18,11 @@ fi
 arg = " ".join(sys.argv[1:])
 if "[ci-skip]" in arg:
     sys.exit(2)
+
+grep_pos = subprocess.run(["grep", "-r", "todo!()", "src"], capture_output=True)
+grep_neg = subprocess.run(["grep", "-r", "//todo!()", "src"], capture_output=True)
+if len(grep_pos.stdout) != 0 and len(grep_neg.stdout) == 0:
+	print(f"todo!() detected:\n{grep_pos.stdout.decode()}")
+	sys.exit(1)
+    
 sys.exit(0)
